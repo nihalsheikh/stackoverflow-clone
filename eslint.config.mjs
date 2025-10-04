a0b08034f55a270b4b07db0f1cad60293406bc3c
nihal <nihalsheikh585@gmail.com>
2025-10-04 11:09:32 +0530

    ADDED: import sorting plugins for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 import tailwindcss from "eslint-plugin-tailwindcss";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,6 +13,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
 	...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+	...tailwindcss.configs["flat/recommended"], // newly added tailwindcss plugin
 	{
 		ignores: [
 			"node_modules/**",
@@ -20,9 +22,40 @@ const eslintConfig = [
 			"build/**",
 			"next-env.d.ts",
 		],
+		// Eslint plugin -> Import sorting
+		plugins: {
+			"simple-import-sort": simpleImportSort,
+		},
+		//  Rules for sorting
+		rules: {
+			"simple-import-sort/imports": [
+				"error",
+				{
+					groups: [
+						// React and Next.js come first
+						["^react", "^next"],
+						// Other external packages
+						["^@?\\w"],
+						// Absolute imports from your project (e.g., @/components)
+						["^@/"],
+						// Relative imports
+						[
+							"^\\.\\.(?!/?$)",
+							"^\\.\\./?$",
+							"^\\./(?=.*/)(?!/?$)",
+							"^\\.(?!/?$)",
+							"^\\./?$",
+						],
+						// Style imports
+						["^.+\\.s?css$"],
+						// Side effect imports
+						["^\\u0000"],
+					],
+				},
+			],
+			"simple-import-sort/exports": "error",
+		},
 	},
-
-	...tailwindcss.configs["flat/recommended"], // newly added tailwindcss plugin
 ];
 
 export default eslintConfig;
