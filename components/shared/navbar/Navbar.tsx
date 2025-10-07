b6b294c1396d51363dec2b01771c635d90bdc519
nihal <nihalsheikh585@gmail.com>
2025-10-07 17:16:31 +0530

    ADDED: Theme Switch Button (working)

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -5,6 +5,8 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
+import Theme from "./Theme";
+
 const Navbar = () => {
   return (
     <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
@@ -22,7 +24,7 @@ const Navbar = () => {
       </Link>
       GlobalSearch
       <div className="flex-between gap-5">
-        ThemeButton
+        <Theme />
         <SignedIn>
           <UserButton
             afterSwitchSessionUrl="/"
---
+++ components/shared/navbar/Theme.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import React from "react";
+
+import Image from "next/image";
+
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+
+import { themes } from "@/constants";
+import { useTheme } from "@/context/ThemeProvider";
+
+const Theme = () => {
+  const { themeMode, setThemeMode } = useTheme();
+
+  return (
+    <Menubar className="relative border-none bg-transparent shadow-none">
+      <MenubarMenu>
+        <MenubarTrigger className="focus:bg-light-900 data-[state=open]:bg-light-900 dark:focus:bg-dark-200 dark:data-[state=open]:bg-dark-200">
+          {themeMode === "light" ? (
+            <Image
+              src="/assets/icons/sun.svg"
+              alt="light-mode"
+              width={20}
+              height={20}
+              className="active-theme"
+            />
+          ) : (
+            <Image
+              src="/assets/icons/moon.svg"
+              alt="dark-mode"
+              width={20}
+              height={20}
+              className="active-theme"
+            />
+          )}
+        </MenubarTrigger>
+        <MenubarContent className="absolute right-[-3rem] mt-3 min-w-[120px] rounded border py-2 dark:border-dark-400 dark:bg-dark-300">
+          {themes.map((theme, index) => (
+            <MenubarItem
+              key={theme.value}
+              className="flex items-center gap-4 px-2.5 py-2 dark:focus:bg-dark-400"
+              onClick={() => {
+                setThemeMode(theme.value);
+
+                if (theme.value !== "system") {
+                  localStorage.theme = theme.value;
+                } else {
+                  localStorage.removeItem("theme");
+                }
+              }}
+            >
+              <Image
+                src={theme.icon}
+                alt={theme.value}
+                width={16}
+                height={16}
+                className={`${themeMode === theme.value && "active-theme"}`}
+              />
+              <p
+                className={`body-semibold text-light-500 ${themeMode === theme.value ? "text-primary-500" : "text-dark-100_light900"}`}
+              >
+                {theme.label}
+              </p>
+            </MenubarItem>
+          ))}
+        </MenubarContent>
+      </MenubarMenu>
+    </Menubar>
+  );
+};
+
+export default Theme;
