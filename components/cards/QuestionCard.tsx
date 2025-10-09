2764eca036b130914b877151c5b4300387274aeb
nihal <nihalsheikh585@gmail.com>
2025-10-09 20:50:00 +0530

    ADDED: QuestionCard Component. MODIFIED: globals.css properties modified

---
+++ components/cards/QuestionCard.tsx
@@ -0,0 +1,100 @@
+import Metric from "../shared/Metric";
+import RenderTag from "../shared/RenderTag";
+
+import Link from "next/link";
+
+import { formatNumber, getTimeStamp } from "@/lib/utils";
+
+interface QuestionCardProps {
+  _id: string;
+  title: string;
+  tags: {
+    _id: string;
+    name: string;
+  }[];
+  author: {
+    _id: string;
+    name: string;
+    picture: string;
+  };
+  createdAt: Date;
+  upvotes: number;
+  answers: Array<object>;
+  views: number;
+}
+
+const QuestionCard = ({
+  _id,
+  title,
+  tags,
+  author,
+  createdAt,
+  upvotes,
+  answers,
+  views,
+}: QuestionCardProps) => {
+  return (
+    <div className="card-wrapper p-9 sm:px-11 rounded-[10px]">
+      <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
+        <div>
+          <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
+            {getTimeStamp(createdAt)}
+          </span>
+          <Link href={`/question/${_id}`}>
+            <h3 className="sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1">
+              {title}
+            </h3>
+          </Link>
+        </div>
+        {/* TODO: If a User is SignedIn, then ADD a Edit and Delete Button Action */}
+      </div>
+
+      {/* Tags */}
+      <div className="mt-3.5 flex flex-wrap gap-2">
+        {tags.map((tag) => (
+          <RenderTag key={tag._id} _id={tag._id} name={tag.name} />
+        ))}
+      </div>
+
+      {/* Author and Post Creation Details */}
+      <div className="flex-between mt-6 w-full flex-wrap gap-3">
+        {/* Metric: Upvotes, Answers, Views */}
+        <Metric // For Author
+          imgUrl="/assets/icons/avatar.svg"
+          alt="user"
+          value={author.name}
+          title={` - asked ${getTimeStamp(createdAt)}`}
+          href={`/profile/${author._id}`}
+          isAuthor
+          textStyles="body-medium text-dark400_light700"
+        />
+
+        <Metric // For Votes
+          imgUrl="/assets/icons/like.svg"
+          alt="upvotes"
+          value={formatNumber(upvotes)}
+          title=" Votes"
+          textStyles="small-medium text-dark400_light800"
+        />
+
+        <Metric // For Answers
+          imgUrl="/assets/icons/message.svg"
+          alt="answers"
+          value={formatNumber(answers.length)}
+          title=" Answers"
+          textStyles="small-medium text-dark400_light800"
+        />
+
+        <Metric // For Views
+          imgUrl="/assets/icons/eye.svg"
+          alt="views"
+          value={formatNumber(views)}
+          title=" Views"
+          textStyles="small-medium text-dark400_light800"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default QuestionCard;
--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -5,51 +5,51 @@ import RenderTag from "./RenderTag";
 
 const hotQuestions = [
   {
-    _id: 1,
+    _id: "1",
     title: "How do I use express as a custom server in NextJS?",
   },
   {
-    _id: 2,
+    _id: "2",
     title: "Cascading Deleted in SQLAlchemy?",
   },
   {
-    _id: 3,
+    _id: "3",
     title: "How to Perfectly center a Div with Tailwind CSS?",
   },
   {
-    _id: 4,
+    _id: "4",
     title:
       "Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)?",
   },
   {
-    _id: 5,
+    _id: "5",
     title: "Redux Toolkit Not Updating State as Expected",
   },
 ];
 
 const popularTags = [
   {
-    _id: 1,
+    _id: "1",
     name: "javascript",
     totalQuestions: 7,
   },
   {
-    _id: 2,
+    _id: "2",
     name: "typescript",
     totalQuestions: 16,
   },
   {
-    _id: 3,
+    _id: "3",
     name: "react",
     totalQuestions: 8,
   },
   {
-    _id: 4,
+    _id: "4",
     name: "next",
     totalQuestions: 12,
   },
   {
-    _id: 5,
+    _id: "5",
     name: "redux",
     totalQuestions: 9,
   },
