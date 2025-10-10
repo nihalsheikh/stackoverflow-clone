f834153effeeff66d5a9bca05e12b5af476ee15a
nihal <nihalsheikh585@gmail.com>
2025-10-10 17:38:28 +0530

    ADDED: Server Action for questionform (work-in-progress)

---
+++ lib/actions/question.action.ts
@@ -0,0 +1,8 @@
+// All the Server Action Code for the Question Model
+"use server";
+
+export async function createQuestion(params) {
+  try {
+    // TODO: connect to DB
+  } catch (error) {}
+}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const withMDX = require("@next/mdx")();
+
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    mdxRs: true,
+  },
+
+  serverExternalPackages: ["mongoose"],
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);
