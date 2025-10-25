5aca575f110e14aaae1ab78aa9c4500eb3168405
nihal <nihalsheikh585@gmail.com>
2025-10-25 10:59:28 +0530

    debugging erros: ESLINT pretteir error fixed, URLProps modified with Promise<{}>

--- types/index.d.ts
+++ types/index.d.ts
@@ -35,8 +35,8 @@ export interface SearchParamsProps {
 }
 
 export interface URLProps {
-  params: { id: string };
-  searchParams: { [key: string]: string | undefined };
+  params: Promise<{ id: string }>;
+  searchParams: Promise<{ [key: string]: string | undefined }>;
 }
 
 export interface BadgeCounts {
