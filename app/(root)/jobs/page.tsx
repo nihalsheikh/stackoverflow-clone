1a0f905b30ad1f8aafbc142ff859dc8164343f36
nihal <nihalsheikh585@gmail.com>
2025-10-30 22:40:02 +0530

    ADDED: jobs page

--- app/(root)/jobs/page.tsx
+++ app/(root)/jobs/page.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 
+import NoResult from "@/components/shared/NoResult";
+
 const page = () => {
-  return <div>Featuring Jobs soon...</div>;
+  return (
+    <div className="">
+      <h1 className="h1-bold text-dark100_light900">Jobs</h1>
+
+      <div className="mt-10 flex flex-col w-full gap-6">
+        <NoResult
+          title="Listing Jobs soon..."
+          description="Page currently in development, you will be notified when the page is live, stay tuned."
+          link="/"
+          buttonTitle="Home"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default page;
